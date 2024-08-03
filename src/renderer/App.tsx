e44ae4ebe81d4349fcb54d10b416b805897b5257
Tegar Levta Anggana <tegarla123@gmail.com>
2024-08-03 22:43:03 +0700

    add fonts

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,20 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 
 function Hello() {
   return (
-    <h1 className='text-4xl'>Hello world</h1>
+    <div>
+      <h1 className="text-4xl font-nunito-sans">Hello world</h1>
+      <h1 className="text-4xl font-manrope">Another Hello</h1>
+      <img
+        src="https://www.w3schools.com/images/lamp.jpg"
+        alt="Lamp"
+        width="32"
+        height="32"
+      />
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,11 @@ module.exports = {
   content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
+    fontFamily: {
+      'nunito-sans': ['Nunito Sans', 'sans-serif'],
+      manrope: ['Manrope', 'sans-serif'],
     },
+    extend: {},
   },
   variants: {
     extend: {},
